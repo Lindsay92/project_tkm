5b0c6aaef4fd76308c00090bc09635f19ea3d4a4
Lindsay <lindsay.dev33@gmail.com>
2022-12-23 17:22:15 +0100

    refactoring code

---
+++ script.js
@@ -0,0 +1,7 @@
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 200 ) { 
+        $('#scrollUp').css('right','10px');
+    } else { 
+        $('#scrollUp').removeAttr( 'style' );
+    }
+});
